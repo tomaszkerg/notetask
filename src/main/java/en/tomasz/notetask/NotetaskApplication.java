2ca29d696e6ce51d1cbda525d5295e0d3807ee86
unknown <tomasz.kerg@gmail.com>
2021-03-10 23:06:17 +0100

    1. first initialization of the project

---
+++ src/main/java/en/tomasz/notetask/NotetaskApplication.java
@@ -0,0 +1,13 @@
+package en.tomasz.notetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotetaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/en/tomasz/notetask/NotetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package en.tomasz.notetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotetaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
