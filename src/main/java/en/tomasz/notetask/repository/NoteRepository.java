0005991a292400d28f01b7aad2d852940b3e61ca
unknown <tomasz.kerg@gmail.com>
2021-03-10 23:41:24 +0100

    4. filling methods in service and preparing database for tests

--- src/main/java/en/tomasz/notetask/repository/NoteRepository.java
+++ src/main/java/en/tomasz/notetask/repository/NoteRepository.java
@@ -5,9 +5,14 @@ import en.tomasz.notetask.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findAllByDeletedIsFalse();
+
+    List<Note> findAllByOriginalId(Long id);
 
 }
--- src/main/java/en/tomasz/notetask/service/NoteService.java
+++ src/main/java/en/tomasz/notetask/service/NoteService.java
@@ -28,29 +28,57 @@ public class NoteService implements NoteServiceI {
 
     @Override
     public NoteDto updateNote(NoteDto noteDto) {
-        return null;
+        Note note = noteRepository.getOne(noteDto.getId());
+        note.setDeleted(true);
+        noteRepository.save(note);
+        Note noteToUpdate = new Note();
+        noteToUpdate.setOriginalId(note.getOriginalId());
+        noteToUpdate.setTitle(noteDto.getTitle());
+        noteToUpdate.setContent(noteDto.getContent());
+        noteToUpdate.setCreated(note.getCreated());
+        noteToUpdate.setDeleted(false);
+        noteToUpdate.setVersion(note.getVersion()+1);
+        Note savedNote = noteRepository.save(noteToUpdate);
+        return NoteConverter.toDto(savedNote);
     }
 
     @Override
     public Optional<NoteDto> findById(Long id) {
-        return null;
+        return noteRepository.findById(id).map(NoteConverter::toDto);
     }
 
     @Override
-    public NoteDto addNote(NoteDto noteDto) {  return null;
+    public NoteDto addNote(NoteDto noteDto) {
+        Note note = Note.builder()
+                .title(noteDto.getTitle())
+                .content(noteDto.getContent())
+                .build();
+        note.setDeleted(false);
+        note.setVersion(1);
+        Note noteToUpdate = noteRepository.save(note);
+        noteToUpdate.setOriginalId(noteToUpdate.getId());
+        noteRepository.save(noteToUpdate);
+        return NoteConverter.toDto(noteRepository.getOne(note.getId()));
     }
 
     @Override
     public List<NoteDto> getNotes() {
-        return null;
+        return noteRepository.findAllByDeletedIsFalse().stream()
+                .map(NoteConverter::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void deleteById(Long id) { ;
+    public void deleteById(Long id) {
+        Note note = noteRepository.getOne(id);
+        note.setDeleted(true);
+        noteRepository.save(note);
     }
 
     @Override
     public List<NoteDto> historyById(Long id) {
-        return null;
+        return noteRepository.findAllByOriginalId(id)
+                .stream().map(NoteConverter::toDto)
+                .collect(Collectors.toList());
     }
 }
